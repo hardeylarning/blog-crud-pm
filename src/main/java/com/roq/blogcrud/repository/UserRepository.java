afaac3e3b0413e5ee28fec80e7f95b6fab2f8045
hardeylarning <hardeylarning@gmail.com>
2023-05-18 09:54:13 +0100

    add: added UserRepository and extended JpaRepository for database interaction

---
+++ src/main/java/com/roq/blogcrud/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.roq.blogcrud.repository;
+
+import com.roq.blogcrud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
